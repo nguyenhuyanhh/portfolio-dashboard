723bb45738aadc251c0979a4bfe65ce532d6e3ba
Huy Anh Nguyen <nguyenhuyanhh@users.noreply.github.com>
2020-05-28 14:52:37 +0800

    Updates

---
+++ precache-manifest.5cd562544a89da02f723ef4675cb9e04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7dc33bd50c76897e702ffccdb93286",
+    "url": "/portfolio-dashboard/index.html"
+  },
+  {
+    "revision": "0676c4aa6004a3f4122d",
+    "url": "/portfolio-dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4fc0ed5a2a633b3576b7",
+    "url": "/portfolio-dashboard/static/js/2.eef07531.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-dashboard/static/js/2.eef07531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0676c4aa6004a3f4122d",
+    "url": "/portfolio-dashboard/static/js/main.8b146e74.chunk.js"
+  },
+  {
+    "revision": "92ab2b1c3303d2150448",
+    "url": "/portfolio-dashboard/static/js/runtime-main.cf25f350.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
